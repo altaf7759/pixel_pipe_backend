1ad0c568d8ab1451114dabdafe3e4d9ab7e19486
Altaf Raja <altaf.1040216@gmail.com>
2026-01-30 17:17:26 +0530

    Increase file upload limit from 5 to 10

--- src/services/multer.js
+++ src/services/multer.js
@@ -3,7 +3,7 @@ import multer from "multer"
 export const upload = multer({
       Storage: multer.memoryStorage(),
       limits: {
-            fileSize: 5 * 1024 * 1024
+            fileSize: 10 * 1024 * 1024
       },
       fileFilter: (req, file, cb) => {
             const allowedTypes = ["image/jpeg", "image/png", "image/webp"];
