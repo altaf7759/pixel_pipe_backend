bc0012bb0f1bc475a2da81d3a81e8a720ee7c43b
Altaf Raja <altaf.1040216@gmail.com>
2026-01-31 14:04:28 +0530

    Fix CORS issue

--- src/index.js
+++ src/index.js
@@ -26,8 +26,11 @@ if (cluster.isPrimary) {
       const PORT = process.env.PORT || 4000
 
       app.use(cors({
-            origin: ["http://localhost:5173", "https://pixel-pipe-frontend.vercel.app"]
-      }))
+            origin: ["http://localhost:5173", "https://pixel-pipe-frontend.vercel.app"],
+            credentials: true,
+            methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+            allowedHeaders: ["Content-Type", "Authorization"]
+      }));
 
       app.get("/", (req, res) => {
             res.json({ status: "ok" });
